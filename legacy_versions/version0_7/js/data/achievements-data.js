e22017c2b21e8a725b0741ebc65082781d3da751
Tyler <122212451+TylerPlayz1987@users.noreply.github.com>
2026-04-02 01:59:34 -0700

    starting making version 0.8.0 and added alpha and beta into the legacy folder (#32)
    * starting make version 0.8.0
    
    * fixed version data
    
    * added alpha and beta builds to the version numbers
    
    * fixed a game crashing bug from  versionalpha and added comments for future reviewers in all legacy files
    
    * fixed 0.4 bug and 0.7 bug and formatted versions alpha - 0.6.0
    
    * fixed change log

---
+++ legacy_versions/version0_7/js/data/achievements-data.js
@@ -0,0 +1,40 @@
+/* LEGACY VERSION NOTICE: Archived build for compatibility/version switching. Known bugs may exist; do not modify gameplay behavior unless required for loading or version navigation. */
+// Shared achievement definitions and icon mapping.
+window.VR_ACHIEVEMENT_DATA = {
+  achievementsTemplate: {
+    floorIsLava: {
+      id: "floorIsLava",
+      name: "Floor is Lava",
+      description: "Die while using the magma theme",
+      unlocked: false,
+      mode: "run",
+    },
+    spacebarCrusher: {
+      id: "spacebarCrusher",
+      name: "Spacebar Crusher",
+      description: "Jump a total of 250 times",
+      unlocked: false,
+      mode: "run",
+    },
+    isThisThingOn: {
+      id: "isThisThingOn",
+      name: "Is This Thing On?",
+      description: "Press jump 50 times without moving at the start of a level",
+      unlocked: false,
+      mode: "run",
+    },
+    staringIntoTheVoid: {
+      id: "staringIntoTheVoid",
+      name: "Staring into the Void",
+      description: "Spend a cumulative 3 minutes on the main menu screen without starting",
+      unlocked: false,
+      mode: "run",
+    },
+  },
+  iconSources: {
+    floorIsLava: "achievement_icons/1.png",
+    spacebarCrusher: "achievement_icons/2.png",
+    isThisThingOn: "achievement_icons/3.png",
+    staringIntoTheVoid: "achievement_icons/5.png",
+  },
+};
---
+++ legacy_versions/version0_7/js/data/version-data.js
@@ -0,0 +1,49 @@
+/* LEGACY VERSION NOTICE: Archived build for compatibility/version switching. Known bugs may exist; do not modify gameplay behavior unless required for loading or version navigation. */
+// Configure this list to add older playable versions.
+// Example old build path: "legacy_versions/version0_1.html"
+window.VR_VERSION_DATA = {
+  currentVersion: "v0.7.0",
+  versions: [
+    {
+      label: "v0.8.0",
+      path: "../../index.html",
+    },
+    {
+      label: "v0.7.0 (Current)",
+      path: "./version0_7.html",
+      current: true,
+    },
+    {
+      label: "v0.6.0",
+      path: "../../legacy_versions/version0_6.html",
+    },
+    {
+      label: "v0.5.0",
+      path: "../../legacy_versions/version0_5.html",
+    },
+    {
+      label: "v0.4.0",
+      path: "../../legacy_versions/version0_4.html",
+    },
+    {
+      label: "v0.3.0",
+      path: "../../legacy_versions/version0_3.html",
+    },
+    {
+      label: "v0.2.0",
+      path: "../../legacy_versions/version0_2.html",
+    },
+    {
+      label: "v0.1.0",
+      path: "../../legacy_versions/version0_1.html",
+    },
+    {
+      label: "v0.0.0-beta",
+      path: "../../legacy_versions/versionbeta.html",
+    },
+    {
+      label: "v0.0.0-alpha",
+      path: "../../legacy_versions/versionalpha.html",
+    },
+  ],
+};
